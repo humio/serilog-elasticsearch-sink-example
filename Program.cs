d1080e88c0cf487ea6950d55ea1a21a0d3aa40f2
fwl <fwl@humio.com>
2019-10-03 13:02:09 +0200

    Initial..

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace serilog_elastic_sink_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerConfig = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:8080/api/v1/ingest/elastic-bulk")) // humio elastic bulk endpoint
+               {
+                   FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                   EmitEventFailure = EmitEventFailureHandling.RaiseCallback,
+                   ModifyConnectionSettings = x => x.BasicAuthentication(username: "", password: "88WS28JEuB0G2WC3cYufdgTINAxOGQvliBrqH5Vqutjb")  // password is ingest token from humio
+               });
+
+            var logger = loggerConfig.CreateLogger();
+
+
+            while (true)
+            {
+                Console.WriteLine("Logging..");
+                logger.Information("This will show up in humio..");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
+
